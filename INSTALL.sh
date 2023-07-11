a2eaffffdd19942671eca2d0b250419b16c0d17f
Steven Patrick Vensko <spvensko@c6145-2-8.local>
2023-07-11 11:44:49 -0400

    Last attempt... again.

--- INSTALL.sh
+++ INSTALL.sh
@@ -19,9 +19,8 @@ echo
 # Create and activate a new environment called ultra
 echo "SETTING UP CONDA ENVIRONMENT"
 echo
-conda create --yes -n ultra python=3.8 pip 
-conda init
-conda activate ultra
+conda create --yes -n base python=3.8 pip 
+conda activate base
 
 # Install uLTRA
 echo
